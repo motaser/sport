818de7f35805f6f714dda0af60977d281f2f6f60
motaser <>
2021-04-14 11:49:41 +0000

    creat javascribt

---
+++ jav/app.js
@@ -0,0 +1,22 @@
+var usernam = 'montaser';
+var age = 26;
+alert('hello in sport worlad')
+
+var userag = prompt('how old are you')
+console.log(userag)
+
+var favoriteSport = prompt('what your favarite sport(football , tennis)')
+
+if(favoriteSport =='football')
+{
+
+var image = '<img src="https://cdn.ttgtmedia.com/visuals/ComputerWeekly/Hero%20Images/sport-football-1-adobe.jpeg" alt= "football"/>';
+
+document.write(image);
+
+}else if(favoriteSport =='tennis')
+{
+  var image2 = '<img src="https://e3arabi.com/wp-content/uploads/2020/01/tennis-3552164_640.jpg" alt= "tennis"/>'
+
+   document.write(image2);
+}
